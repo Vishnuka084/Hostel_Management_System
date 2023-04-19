b66f306b3e625d0384c33a44bfb320be58c5aef2
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-04-19 16:30:10 +0530

    Add QueryDAOImpl to Genarate Join Queries & Add App java class

---
+++ src/App.java
@@ -0,0 +1,27 @@
+import lk.ijse.hibernate.util.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("SELECT s.student_id,  " +
+                "ss.res_id,s.name,ss.status,ss.date FROM Student s INNER JOIN Reservation ss" +
+                " ON s.student_id = ss.student.student_id WHERE ss.status= : y");
+
+        query.setParameter("y","Paid Later");
+
+        List<Object[]> list = query.list();
+        for (Object[] objects:list
+        ) {
+            System.out.println(objects[0]+" "+objects[1]+" "+objects[2]+" "+objects[3]+" "+objects[4]);
+        }
+
+        transaction.commit();
+        session.close();
+    }
+}
---
+++ src/lk/ijse/hibernate/dao/custom/QueryDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.hibernate.dao.custom;
+
+import java.util.List;
+
+public interface QueryDAO {
+    List<Object []> getAllPendingKeyMoneyReservationsUsingReservationStatus(String status) throws Exception;
+}
